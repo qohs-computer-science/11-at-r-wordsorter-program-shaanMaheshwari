28b6fa61a59177e1e7ec7d6ec640b927fd41924c
shaanMaheshwari <ShaanMaheshwari2006@gmail.com>
2024-10-02 14:17:25 +0000

    working - 10/2

--- WordSorter.java
+++ WordSorter.java
@@ -1,36 +1,51 @@
 import java.io.File;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class WordSorter 
 {
-	public static void main(String[] args) 
-	{
-		Scanner in = new Scanner(System.in); 
-		try
+	//boolean keepRunning = true;
+	//while(keepRunning == true){
+
+		public static void main(String[] args) 
 		{
-			in = new Scanner(new File("article.txt"));	
-		}
-		catch(Exception e)
-		{
-			System.out.println("Cannot find file... Exiting Program");
-			return;
-		}	
-		String word = "";
-		while(in.hasNext())
-		{
-			word = in.next();
-			word = word.replace(",", "");
-			word = word.replace("(tm)", "");
-			word = word.replace(")", "");
-			word = word.replace("(", "");
-			word = word.replace(".", "");
-			word = word.replace("'s", "");
-			word = word.replace(":", "");
-			word = word.replace("!", "");
-			word = word.replace("?", "");
-			word = word.replace("'", "");
-			System.out.println(word);
-			
-			
+			Scanner in = new Scanner(System.in); 
+
+			ArrayList<String>[] wordsList = new ArrayList[26];
+
+			for(int i = 0; i < wordsList.length; i++){
+				wordsList[i] = new ArrayList<String>();
+			}
+
+			try
+			{
+				in = new Scanner(new File("article.txt"));	
+			}
+			catch(Exception e)
+			{
+				System.out.println("Cannot find file... Exiting Program");
+				return;
+			}	
+			String word = "";
+			while(in.hasNext())
+			{
+				word = in.next();
+				word = word.replace(",", "");
+				word = word.replace("(tm)", "");
+				word = word.replace(")", "");
+				word = word.replace("(", "");
+				word = word.replace(".", "");
+				word = word.replace("'s", "");
+				word = word.replace(":", "");
+				word = word.replace("!", "");
+				word = word.replace("?", "");
+				word = word.replace("'", "");
+				System.out.println(word);
+				word = word.toLowerCase();
+
+				int currLoc = word.charAt(0) - 97;
+				wordsList[currLoc].add(word);
+			}
 		}
 	}
-}
+//}
